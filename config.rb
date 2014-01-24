46d2df0c405eab070e95729476e52274df9a9e24
Vladimir Bazhanov <vladimir@bazhanov.org>
2014-01-24 11:33:48 +0200

    Text about WiFi changed

--- config.rb
+++ config.rb
@@ -4,16 +4,9 @@ set :images_dir, 'assets/images'
 set :fonts_dir, 'assets/fonts'
 set :partials_dir, 'partials'
 
-#set :css_dir, 'stylesheets'
-#set :js_dir, 'javascripts'
-#set :images_dir, 'images'
-
-
 activate :deploy do |deploy|
   deploy.method = :git
 end
-
-# Build-specific configuration
 
 configure :build do
   activate :minify_css
